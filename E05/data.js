ee4fde95fbe3d9c96660eb2298ff9fd12efb4e7a
Maurice Lucas <mauricelucas@MacBook-Pro.fritz.box>
2019-04-28 18:01:38 +0200

    Upload E05

---
+++ E05/data.js
@@ -0,0 +1,22 @@
+var E05;
+(function (E05) {
+    E05.products = [
+        { type: "Waffelart", name: "Waffel", price: 0.10 },
+        { type: "Waffelart", name: "Becher", price: 0.10 },
+        { type: "Eiskugel", name: "Vanille", price: 0.70 },
+        { type: "Eiskugel", name: "Schokolade", price: 1.20 },
+        { type: "Eiskugel", name: "Erdbeer", price: 1.20 },
+        { type: "Eiskugel", name: "Stracciatella", price: 1.20 },
+        { type: "Eiskugel", name: "Vodka Lemon", price: 1.20 },
+        { type: "Eiskugel", name: "Schlumpf", price: 1.20 },
+        { type: "Topping", name: "kein Topping", price: 0.00 },
+        { type: "Topping", name: "Sahne", price: 1.20 },
+        { type: "Topping", name: "Schokostreusel", price: 1.40 },
+        { type: "Topping", name: "Bunte Streusel", price: 1.20 },
+        { type: "Topping", name: "Smarties", price: 1.30 },
+        { type: "Topping", name: "Erdbeersoße", price: 1.20 },
+        { type: "Topping", name: "Bananasplit", price: 1.10 },
+        { type: "Shipping", name: "Standart Lieferung", price: 2.00 },
+        { type: "Shipping", name: "Express Lieferung", price: 5.00 }
+    ];
+})(E05 || (E05 = {}));
