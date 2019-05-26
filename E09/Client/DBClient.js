ed08553a94c2f2d7bd3de61961ff83043c5104db
Maurice Lucas <mauricelucas@MacBook-Pro.local>
2019-05-26 16:55:15 +0200

    Upload E09

---
+++ E09/Client/DBClient.js
@@ -0,0 +1,55 @@
+var DBClient;
+(function (DBClient) {
+    window.addEventListener("load", init);
+    //let serverAddress: string = "http://localhost:8100/";
+    var serverAddress = "https://eia2-testserver.herokuapp.com/";
+    function init(_event) {
+        console.log("Init");
+        var insertButton = document.getElementById("insert");
+        var refreshButton = document.getElementById("refresh");
+        var findButton = document.getElementById("find");
+        insertButton.addEventListener("click", insert);
+        refreshButton.addEventListener("click", refresh);
+        findButton.addEventListener("click", find);
+    }
+    function insert(_event) {
+        var inputs = document.getElementsByTagName("input");
+        var query = "command=insert";
+        query += "&name=" + inputs[0].value;
+        query += "&firstname=" + inputs[1].value;
+        query += "&matrikel=" + inputs[2].value;
+        console.log(query);
+        sendRequest(query, handleInsertResponse);
+    }
+    function refresh(_event) {
+        var query = "command=refresh";
+        sendRequest(query, handleFindResponse);
+    }
+    function sendRequest(_query, _callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", serverAddress + "?" + _query, true);
+        xhr.addEventListener("readystatechange", _callback);
+        xhr.send();
+    }
+    function handleInsertResponse(_event) {
+        var xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert(xhr.response);
+        }
+    }
+    function find(_event) {
+        var matrikelFind = Number(document.getElementById("matrikelFinder").value);
+        console.log(matrikelFind);
+        var query = 'command=find&matrikel=${matrikelFind}';
+        sendRequest(query, handleFindResponse);
+    }
+    function handleFindResponse(_event) {
+        var xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            var output = document.getElementsByTagName("textarea")[0];
+            output.value = xhr.response;
+            var responseAsJson = JSON.parse(xhr.response);
+            console.log(responseAsJson);
+        }
+    }
+})(DBClient || (DBClient = {}));
