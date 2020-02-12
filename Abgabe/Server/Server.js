f944965705dc2703f90c10af77e0518f7d8bb41c
Maurice Lucas <mauricelucas@MacBook-Pro.fritz.box>
2020-02-12 19:24:57 +0100

    Upload

--- Abgabe/Server/Server.js
+++ Abgabe/Server/Server.js
@@ -18,7 +18,7 @@ var Endabgabe;
     if (port == undefined) {
         port = 5001;
     }
-    let databaseUrl = "mongodb+srv://mo:mo123å@eiatest-8zhhe.mongodb.net/test?retryWrites=true&w=majority";
+    let databaseUrl = "mongodb+srv://mo:mo123@eiatest-8zhhe.mongodb.net/test?retryWrites=true&w=majority";
     connectToDatabase(databaseUrl);
     startServer(port);
     function startServer(_port) {
