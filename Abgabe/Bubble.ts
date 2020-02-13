70797fff52f9cd943008c1a6c32f3a82f022be82
Maurice Lucas <mauricelucas@MacBook-Pro.fritz.box>
2020-02-13 10:16:17 +0100

    upload

--- Abgabe/Bubble.ts
+++ Abgabe/Bubble.ts
@@ -22,7 +22,7 @@ namespace Endabgabe {
             if (this.position.y < 0)
                 this.position.y += crc2.canvas.height;
 
-            if (this.position.x < 600)
+            if (this.position.x < 0)
                 this.position.x += crc2.canvas.width;
         }
 
